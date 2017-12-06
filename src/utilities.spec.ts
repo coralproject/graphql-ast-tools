be954f939f6c463b3e91bb50ead0f4807e7e9380
Chi Vinh Le <vinh@wikiwi.io>
2017-12-06 14:35:29 +0100

    Add more test coverage

--- src/utilities.spec.ts
+++ src/utilities.spec.ts
@@ -1,5 +1,7 @@
 import {
   getDefinitionID,
+  mergeDefinitions,
+  mergeSelectionSets,
 } from "./utilities";
 
 import { assert } from "chai";
@@ -9,6 +11,37 @@ import { assert } from "chai";
 describe("utilities.ts", () => {
   describe("getDefinitionID", () => {
     it("should return definition ID", () => {
+      const testcases = [
+        {
+          ast: {
+            "kind": "Field",
+            "name": { "kind": "Name", "value": "name" },
+            "alias": { "kind": "Name", "value": "aliased" },
+          },
+          expected: "Field_aliased",
+        },
+        {
+          ast: {
+            "kind": "InlineFragment",
+            "typeCondition": { "kind": "NamedType", "name": { "kind": "Name", "value": "Person" } },
+          },
+          expected: "InlineFragment_Person",
+        },
+        {
+          ast: {
+            "kind": "FragmentSpread",
+            "name": { "kind": "Name", "value": "Person" },
+          },
+          expected: "FragmentSpread_Person",
+        },
+      ];
+
+      testcases.forEach(({ ast, expected }) => {
+        assert.strictEqual(getDefinitionID(ast), expected);
+      });
+    });
+
+    it("should return definition ID with directives", () => {
       const directivesAST = {
         "kind": "Field",
         "alias": null,
@@ -68,5 +101,188 @@ describe("utilities.ts", () => {
       const expected = "Field_name_@include(if:$withFriends)@include(if:true)";
       assert.strictEqual(result, expected);
     });
+  });
+  describe("mergeDefinitions", () => {
+    it("should merge definition leafs", () => {
+      const astA = {
+        "kind": "Field",
+        "name": { "kind": "Name", "value": "hero" },
+        "selectionSet": {
+          "kind": "SelectionSet",
+          "selections": [
+            {
+              "kind": "Field",
+              "name": { "kind": "Name", "value": "name" },
+            },
+            {
+              "kind": "Field",
+              "name": { "kind": "Name", "value": "title" },
+            },
+          ]
+        }
+      };
+
+      const astB = {
+        "kind": "Field",
+        "name": { "kind": "Name", "value": "hero" },
+        "selectionSet": {
+          "kind": "SelectionSet",
+          "selections": [
+            {
+              "kind": "Field",
+              "name": { "kind": "Name", "value": "name" },
+            },
+            {
+              "kind": "Field",
+              "name": { "kind": "Name", "value": "age" },
+            },
+          ]
+        }
+      };
+
+      const expected = {
+        "kind": "Field",
+        "name": { "kind": "Name", "value": "hero" },
+        "selectionSet": {
+          "kind": "SelectionSet",
+          "selections": [
+            {
+              "kind": "Field",
+              "name": { "kind": "Name", "value": "name" },
+            },
+            {
+              "kind": "Field",
+              "name": { "kind": "Name", "value": "title" },
+            },
+            {
+              "kind": "Field",
+              "name": { "kind": "Name", "value": "age" },
+            },
+          ]
+        }
+      };
+
+      const result = mergeDefinitions(astA, astB);
+      assert.deepEqual(result, expected);
+    });
+
+    it("should merge definition deeply", () => {
+      const astA = {
+        "kind": "Field",
+        "name": { "kind": "Name", "value": "hero" },
+        "selectionSet": {
+          "kind": "SelectionSet",
+          "selections": [
+            {
+              "kind": "Field",
+              "name": { "kind": "Name", "value": "name" },
+            },
+            {
+              "kind": "Field",
+              "name": { "kind": "Name", "value": "title" },
+            },
+            {
+              "kind": "Field",
+              "name": { "kind": "Name", "value": "partner" },
+              "selectionSet": {
+                "kind": "SelectionSet",
+                "selections": [
+                  {
+                    "kind": "Field",
+                    "name": { "kind": "Name", "value": "name" },
+                  },
+                  {
+                    "kind": "Field",
+                    "name": { "kind": "Name", "value": "title" },
+                  },
+                ],
+              }
+            }
+          ],
+        }
+      };
+
+      const astB = {
+        "kind": "Field",
+        "name": { "kind": "Name", "value": "hero" },
+        "selectionSet": {
+          "kind": "SelectionSet",
+          "selections": [
+            {
+              "kind": "Field",
+              "name": { "kind": "Name", "value": "name" },
+            },
+            {
+              "kind": "Field",
+              "name": { "kind": "Name", "value": "age" },
+            },
+            {
+              "kind": "Field",
+              "name": { "kind": "Name", "value": "partner" },
+              "selectionSet": {
+                "kind": "SelectionSet",
+                "selections": [
+                  {
+                    "kind": "Field",
+                    "name": { "kind": "Name", "value": "name" },
+                  },
+                  {
+                    "kind": "Field",
+                    "name": { "kind": "Name", "value": "age" },
+                  },
+                ],
+              }
+            }
+          ]
+        }
+      };
+
+      const expected = {
+        "kind": "Field",
+        "name": { "kind": "Name", "value": "hero" },
+        "selectionSet": {
+          "kind": "SelectionSet",
+          "selections": [
+            {
+              "kind": "Field",
+              "name": { "kind": "Name", "value": "name" },
+            },
+            {
+              "kind": "Field",
+              "name": { "kind": "Name", "value": "title" },
+            },
+            {
+              "kind": "Field",
+              "name": { "kind": "Name", "value": "partner" },
+              "selectionSet": {
+                "kind": "SelectionSet",
+                "selections": [
+                  {
+                    "kind": "Field",
+                    "name": { "kind": "Name", "value": "name" },
+                  },
+                  {
+                    "kind": "Field",
+                    "name": { "kind": "Name", "value": "title" },
+                  },
+                  {
+                    "kind": "Field",
+                    "name": { "kind": "Name", "value": "age" },
+                  },
+                ],
+              }
+            },
+            {
+              "kind": "Field",
+              "name": { "kind": "Name", "value": "age" },
+            },
+          ]
+        }
+      };
+
+      const result = mergeDefinitions(astA, astB);
+      assert.deepEqual(result, expected);
+    });
+
   });
 });
